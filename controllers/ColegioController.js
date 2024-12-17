8dcf9565bc2183b2dd6c2f0a7015b12f0c4356cb
EduardoGarcia360 <eduardo_garcia360@hotmail.com>
2024-12-17 16:07:30 -0600

    controller y routes colegio

---
+++ controllers/ColegioController.js
@@ -0,0 +1,64 @@
+import Colegio from "../models/Colegio.js";
+
+export const getAllColegios = async (req, res) => {
+    try {
+        const colegios = await Colegio.findAll();
+        res.status(200).json(colegios);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
+
+export const getColegio = async (req, res) => {
+    try {
+        const colegio = await Colegio.findAll({
+            where: {
+                id_colegio: req.params.id
+            }
+        });
+        res.status(200).json(colegio[0]);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
+
+export const createColegio = async (req, res) => {
+    try {
+        await Colegio.create(req.body);
+        res.status(200).json({ message: 'Colegio creado correctamente!' });
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
+
+export const updateColegio = async (req, res) => {
+    try {
+        await Colegio.update(req.body, {
+            where: {
+                id_colegio: req.params.id
+            }
+        });
+        res.status(200).json({ message: 'Colegio actualizado correctamente!' });
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
+
+export const deleteColegio = async (req, res) => {
+    try {
+        const colegio = await Colegio.findOne({ where: { id_colegio: req.params.id } });
+
+        if (!colegio) {
+            return res.status(400).json({ message: 'Colegio no encontrado' });
+        }
+
+        await Colegio.destroy({
+            where: {
+                id_colegio: req.params.id
+            }
+        });
+        res.status(200).json({ message: 'Colegio eliminado correctamente!' });
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { getAllUsuarios, getUsuario, createUsuario, updateUsuario, deleteUsuario } from "../controllers/UsuarioController.js";
+import { getAllColegios, getColegio, createColegio, updateColegio, deleteColegio } from "../controllers/ColegioController.js";
 const router = express.Router()
 
 // usuarios
@@ -8,5 +9,12 @@ router.get('/usuarios/:id', getUsuario)
 router.post('/usuarios/', createUsuario)
 router.put('/usuarios/:id', updateUsuario)
 router.delete('/usuarios/:id', deleteUsuario)
+
+// colegios
+router.get('/colegios/', getAllColegios);
+router.get('/colegios/:id', getColegio);
+router.post('/colegios/', createColegio);
+router.put('/colegios/:id', updateColegio);
+router.delete('/colegios/:id', deleteColegio);
 
 export default router
