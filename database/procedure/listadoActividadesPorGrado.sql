90f5e081e7adfd536c47e2eca5a7ec3bb779a063
EduardoGarcia360 <eduardo_garcia360@hotmail.com>
2025-01-16 12:26:44 -0600

    upd sp

--- database/procedure/listadoActividadesPorGrado.sql
+++ database/procedure/listadoActividadesPorGrado.sql
@@ -1,4 +1,4 @@
-CREATE PROCEDURE escritorio_academico.listadoActividadesPorGrado(IN p_id_colegio INT,
+CREATE DEFINER=`root`@`localhost` PROCEDURE `escritorio_academico`.`listadoActividadesPorGrado`(IN p_id_colegio INT,
     IN p_id_grado INT)
 begin
 	-- Listar los gastos extraordinarios asignados a un grado específico
@@ -7,7 +7,13 @@ begin
            AGE.monto,
            AGE.descripcion,
            AGE.fecha_asignacion,
-           GE.nombre_gasto
+           GE.nombre_gasto,
+           GE.es_actividad_bus,
+           (
+           	select COUNT(1)
+           	from AsignacionTransporteExtra ate
+           	where ate.id_asignacion = AGE.id_asignacion
+           ) cantidad_buses
     FROM AsignacionGastoExtra AGE
     INNER JOIN GastoExtraordinario GE ON AGE.id_gasto = GE.id_gasto
     WHERE GE.id_colegio = p_id_colegio
