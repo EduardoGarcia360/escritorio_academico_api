3ce4c9c012547b8963aeb787a8c15f9b5796c92b
EduardoGarcia360 <eduardo_garcia360@hotmail.com>
2025-02-19 11:41:03 -0600

    cambio credenciales

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 export const PORT = process.env.PORT || 3000;
 
-export const DB_HOST = 'mysql.railway.internal';
-export const DB_PORT = 3306;
+export const DB_HOST = 'shortline.proxy.rlwy.net';
+export const DB_PORT = 33655;
 export const DB_NAME = 'railway';
 export const DB_USER = 'root';
 export const DB_PASSWORD = 'VzNinFRTQfjHVMDbPoaGCKNeiAgASRqN';
