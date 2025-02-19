a242698c10cf9efd1f4e2144e17bc92dcc0e57b9
EduardoGarcia360 <eduardo_garcia360@hotmail.com>
2025-02-19 11:00:36 -0600

    log config error

--- app.js
+++ app.js
@@ -5,7 +5,14 @@ import cookieParser from 'cookie-parser';
 import { db } from "./models/index.js";
 import escritorioRoutes from "./routes/routes.js";
 import encryptResponse from "./middlewares/responseEncryptMiddleware.js";
-import { PORT } from "./config.js";
+import { 
+    PORT,
+    DB_HOST,
+    DB_PORT,
+    DB_NAME,
+    DB_USER,
+    DB_PASSWORD,
+} from "./config.js";
 
 dotenv.config({ path: './development.env' });
 const app = express()
@@ -24,6 +31,7 @@ try {
     console.log('conexión a la base')
 } catch (error) {
     console.error(`error de conexión en bd: ${error}`)
+    console.error(`host: ${DB_HOST}, port: ${DB_PORT}, name: ${DB_NAME}, user: ${DB_USER}, password: ${DB_PASSWORD}`)
 }
 
 app.listen(8000, () => {
