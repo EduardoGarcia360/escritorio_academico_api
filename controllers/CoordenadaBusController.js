79464d34e2f1bb4af2e3c5448c4fdd77e50ee697
EduardoGarcia360 <eduardo_garcia360@hotmail.com>
2025-02-28 20:16:23 -0600

    fix campos coordenada bus

--- controllers/CoordenadaBusController.js
+++ controllers/CoordenadaBusController.js
@@ -1,4 +1,5 @@
 import CoordenadaBus from "../models/CoordenadaBus.js";
+import { decodeJWT } from "../utils/codificar.js";
 
 export const getAllCoordenadasBus = async (req, res) => {
     try {
@@ -42,6 +43,15 @@ export const getCoordenadaBus = async (req, res) => {
 
 export const createCoordenadaBus = async (req, res) => {
     try {
+        const token = req.cookies?.token;
+        const userData = decodeJWT(token);
+
+        if (!userData) {
+            return res.status(403).json({ status: 'ERROR', message: "Token inválido o no proporcionado" });
+        }
+
+        req.body.id_usuario_creo = userData.id;
+        
         const nuevaCoordenada = await CoordenadaBus.create(req.body);
         res.status(200).json({ message: 'Coordenada creada correctamente!', coordenada: nuevaCoordenada });
     } catch (error) {
