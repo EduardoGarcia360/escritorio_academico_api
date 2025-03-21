7866c735406badae4f149cce5e613faca2f263a7
EduardoGarcia360 <eduardo_garcia360@hotmail.com>
2025-03-20 18:23:00 -0600

    fix logica tutor y usuario tutor

--- controllers/TutorController.js
+++ controllers/TutorController.js
@@ -1,6 +1,10 @@
 import Tutor from "../models/Tutor.js";
 import TutorEstudiante from "../models/TutorEstudiante.js";
+import UsuarioTutor from "../models/UsuarioTutor.js";
+import Usuario from "../models/Usuario.js";
 import { decodeJWT } from "../utils/codificar.js";
+import bcrypt from "bcrypt";
+import { Op } from "sequelize";
 
 export const getAllTutores = async (req, res) => {
     try {
@@ -93,15 +97,15 @@ export const createTutor = async (req, res) => {
             return res.status(403).json({ status: 'ERROR', message: "Token inválido o no proporcionado" });
         }
 
-        // Asignar los valores del usuario que creó el tutor
-        req.body.id_usuario_creo = userData.id;
-        req.body.id_colegio = userData.id_colegio;
-
-        // Crear el tutor
-        const tutor = await Tutor.create(req.body);
-
         // Crear la relación TutorEstudiante
         if (req.body.id_estudiante) {
+            // Asignar los valores del usuario que creó el tutor
+            req.body.id_usuario_creo = userData.id;
+            req.body.id_colegio = userData.id_colegio;
+
+            // Crear el tutor
+            const tutor = await Tutor.create(req.body);
+
             await TutorEstudiante.create({
                 id_tutor: tutor.id_tutor,
                 id_estudiante: req.body.id_estudiante,
@@ -109,9 +113,31 @@ export const createTutor = async (req, res) => {
                 es_responsable_principal: req.body.es_responsable_principal || false, // Valor por defecto si no se envía
                 id_usuario_creo: req.body.id_usuario_creo
             });
+
+            // Crear el usuario del tutor
+            const userParams = {
+                nombre_usuario: req.body.correo_electronico,
+                contrasena: req.body.contrasena,
+                nombre_completo: req.body.nombre_completo,
+                correo_electronico: req.body.correo_electronico,
+                rol: 'P', // Padre de familia
+                estado: 'A',
+            }
+
+            const usuario = await Usuario.create(userParams);
+
+            // Crear la relación en UsuarioTutor
+            await UsuarioTutor.create({
+                id_usuario: usuario.id_usuario,
+                id_tutor: tutor.id_tutor,
+                id_usuario_creo: userData.id
+            });
+
+            res.status(200).json({ status: 'OK', message: 'Tutor y relación creados correctamente!' });
+        } else {
+            return res.status(400).json({ status: 'ERROR', message: "No hay estudiante asociado" });
         }
 
-        res.status(200).json({ status: 'OK', message: 'Tutor y relación creados correctamente!' });
     } catch (error) {
         res.status(400).json({ status: 'ERROR', message: error.message });
     }
@@ -128,6 +154,46 @@ export const updateTutor = async (req, res) => {
 
         req.body.id_usuario_modifico = userData.id
 
+        // Validar si el correo_electronico ya existe en otros registros
+        if (req.body.correo_electronico) {
+            const correoExistente = await Tutor.findOne({
+                where: {
+                    correo_electronico: req.body.correo_electronico,
+                    id_tutor: { [Op.ne]: req.params.id } // Excluir el tutor actual
+                }
+            });
+            // console.log('correo de tutor222', correoExistente)
+            if (correoExistente) {
+                return res.status(400).json({ status: 'ERROR', message: "Correo ya en uso" });
+            }
+        }
+
+        // console.log('contrasena', req.body.contrasena)
+        if (req.body.contrasena) {
+            // si el usuario envio un cambio de contraseña
+            // Encriptar la nueva contraseña
+            const salt = await bcrypt.genSalt(10);
+            req.body.contrasena = await bcrypt.hash(req.body.contrasena, salt);
+        }
+
+        const objUsuarioTutor = await UsuarioTutor.findOne({
+            where: { id_tutor: req.params.id }
+        });
+
+        // console.log('validacion usuario', req.body.correo_electronico, objUsuarioTutor)
+        if (req.body.correo_electronico) {
+            const correoExistente = await Usuario.findOne({
+                where: {
+                    correo_electronico: req.body.correo_electronico,
+                    id_usuario: { [Op.ne]: objUsuarioTutor.id_usuario } // Excluir el usuario actual
+                }
+            });
+            // console.log('correo de usuario', correoExistente)
+            if (correoExistente) {
+                return res.status(400).json({ status: 'ERROR', message: "Correo ya en uso" });
+            }
+        }
+
         await Tutor.update(req.body, {
             where: {
                 id_tutor: req.params.id
@@ -142,8 +208,28 @@ export const updateTutor = async (req, res) => {
                 id_tutor: req.params.id
             }
         })
+
+        // Actualizar el usuario del tutor
+        let userParams = {
+            nombre_usuario: req.body.correo_electronico,
+            nombre_completo: req.body.nombre_completo,
+            correo_electronico: req.body.correo_electronico,
+            id_usuario_modifico: userData.id,
+        }
+
+        if (req.body.contrasena) {
+            userParams.contrasena = req.body.contrasena;
+        }
+
+        await Usuario.update(userParams, {
+            where: {
+                id_usuario: objUsuarioTutor.id_usuario
+            }
+        });
+        
         res.status(200).json({ status: 'OK', message: 'Tutor actualizado correctamente!' });
     } catch (error) {
+        console.log('ERROR', error)
         res.status(400).json({ status: 'ERROR', message: error.message });
     }
 };
